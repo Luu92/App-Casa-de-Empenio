0cf4da0ff01beb131a0cc4dafe77b3b0dc0dcb4b
DevLuu <luu.vale.05@gmail.com>
2021-12-07 21:12:04 -0600

    Se hizo la primera vista login
    inicializamos la aplicación separando por componentes

---
+++ src/mx/edu/uacm/AModSoft/CasadeEmpeño/CasaEmpeño.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.uacm.AModSoft.CasadeEmpeño;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Internet
+ */
+public class CasaEmpeño extends Application {
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/mx/edu/uacm/vistasFXML/login.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            arg0.setScene(scene);
+            arg0.setTitle("Sistema Casa de Empeño");
+            arg0.setResizable(false);
+            arg0.getIcons().add(new Image(CasaEmpeño.class.getResourceAsStream("/mx/edu/uacm/img/intercambio.png")));
+            arg0.show();
+        } catch (IOException error) {
+            System.out.println("Error al cargar la vista " + error);
+        }
+
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/mx/edu/uacm/controladoresFXML/LoginController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.uacm.controladoresFXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Internet
+ */
+public class LoginController implements Initializable {
+
+    
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private TextField etiquetaUsuario;
+    @FXML
+    private PasswordField etiquetaContra;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    public void mostrarMensaje(){
+        System.out.println(etiquetaContra.getText());
+    }
+}
