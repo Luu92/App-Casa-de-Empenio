5e03b2a80ca08b8c18009d7f41cab3e1b35125cc
DevLuu <luu.vale.05@gmail.com>
2021-12-07 21:24:47 -0600

    Update LoginController.java

--- src/mx/edu/uacm/controladoresFXML/LoginController.java
+++ src/mx/edu/uacm/controladoresFXML/LoginController.java
@@ -10,7 +10,6 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
