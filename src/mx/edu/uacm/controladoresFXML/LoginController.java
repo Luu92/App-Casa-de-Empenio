fda240d13c4c5cca6858ddaae69140a4b9c53556
DevLuu <luu.vale.05@gmail.com>
2021-12-10 21:41:17 -0600

    Agregando interaccion entre vistas

--- src/mx/edu/uacm/controladoresFXML/LoginController.java
+++ src/mx/edu/uacm/controladoresFXML/LoginController.java
@@ -5,16 +5,16 @@
  */
 package mx.edu.uacm.controladoresFXML;
 
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -46,22 +46,28 @@ public class LoginController implements Initializable {
         // TODO
     }    
     
-    @FXML
-    public void mostrarMensaje(){
+    public void mostrarMensaje(ActionEvent event){
         Pattern expresion = Pattern.compile("^[a-z]{5}[0-9]{1,2}");
         Matcher validator = expresion.matcher(etiquetaUsuario.getText());
         if(!etiquetaUsuario.getText().isEmpty() && validator.matches() && !etiquetaContra.getText().isEmpty()){
             /*En esta parte metemos la vista*/
             try {
+                
+                Parent root = FXMLLoader.load(getClass().getResource("menuFXML.fxml"));
+                Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+                /*
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("menuFXML.fxml"));
                 Parent root = loader.load();
-                MenuFXMLController ventanaMenu = (MenuFXMLController)loader.getController();
+               // MenuFXMLController ventanaMenu = (MenuFXMLController)loader.getController();
                 Scene scene = new Scene(root);
                 Stage stage = new Stage();
                 stage.setScene(scene);
                 stage.show();
                 Stage nuevoStage = (Stage) btnLogin.getScene().getWindow();
-                nuevoStage.close();
+                nuevoStage.close();*/
             } catch (IOException ex) {
                 ex.printStackTrace(System.out);
             }
@@ -71,5 +77,40 @@ public class LoginController implements Initializable {
             
             JOptionPane.showMessageDialog(null,"Revisar Usuario y Contraseña","Error", JOptionPane.WARNING_MESSAGE);
         }     
+    }
+
+    @FXML
+    private void mostrarMensaje(javafx.event.ActionEvent event) {
+        Pattern expresion = Pattern.compile("^[a-z]{5}[0-9]{1,2}");
+        Matcher validator = expresion.matcher(etiquetaUsuario.getText());
+        if(!etiquetaUsuario.getText().isEmpty() && validator.matches() && !etiquetaContra.getText().isEmpty()){
+            /*En esta parte metemos la vista*/
+            try {
+                
+                Parent root =  FXMLLoader.load(getClass().getClassLoader().getResource("/mx/edu/uacm/vistasFXML/menuFXML.fxml"));
+                Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+                /*
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("menuFXML.fxml"));
+                Parent root = loader.load();
+               // MenuFXMLController ventanaMenu = (MenuFXMLController)loader.getController();
+                Scene scene = new Scene(root);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+                Stage nuevoStage = (Stage) btnLogin.getScene().getWindow();
+                nuevoStage.close();*/
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            JOptionPane.showMessageDialog(null,"Iniciando Sesión");
+        }
+        else{
+            
+            JOptionPane.showMessageDialog(null,"Revisar Usuario y Contraseña","Error", JOptionPane.WARNING_MESSAGE);
+        }  
+        
     }
 }
